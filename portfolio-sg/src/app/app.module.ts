5f5a8f39204752cb0c0a788f2ec01982d92f9dc6
Danijel Malinovic <danijelmalinovic2@gmail.com>
2020-11-19 21:20:37 +0100

    created header

--- portfolio-sg/src/app/app.module.ts
+++ portfolio-sg/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ComponentLibraryComponent } from './pages/component-library/component-library.component';
+import { HeaderComponent } from './components/organisms/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ComponentLibraryComponent
+    ComponentLibraryComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

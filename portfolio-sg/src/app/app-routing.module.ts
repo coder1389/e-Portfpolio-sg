143d39c50ae80cf2b38795d94b467d08a477bc83
Danijel Malinovic <danijelmalinovic2@gmail.com>
2020-11-19 21:19:41 +0100

    zwischenstand

--- portfolio-sg/src/app/app-routing.module.ts
+++ portfolio-sg/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComponentLibraryComponent } from './pages/component-library/component-library.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'component-library',
+    component: ComponentLibraryComponent
+  }, 
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

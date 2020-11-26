a4d500b0d9b19d3c3e89d97a65c71101f7fbdb25
Danijel Malinovic <danijelmalinovic2@gmail.com>
2020-11-26 15:23:16 +0100

    changes

--- portfolio-sg/src/app/components/atoms/icon/icon.component.ts
+++ portfolio-sg/src/app/components/atoms/icon/icon.component.ts
@@ -3,8 +3,15 @@ import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 @Component({
   selector: 'sg-icon',
   templateUrl: './icon.component.html',
-  styleUrls: ['./icon.component.scss']
+  styleUrls: ['./icon.component.scss'],
 })
 export class IconComponent {
-  @Input() name: 'arrow_up' | 'article' | 'home' | 'perm' | 'visibility';
+  @Input() name:
+    | 'arrow_up'
+    | 'article'
+    | 'home'
+    | 'perm'
+    | 'visibility'
+    | 'facebook'
+    | 'github';
 }
--- portfolio-sg/src/app/components/organisms/footer/footer.component.ts
+++ portfolio-sg/src/app/components/organisms/footer/footer.component.ts
@@ -8,6 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
   yearIndication: string;
 
+  readonly externalLinks = [
+    {
+      name: 'GitHub',
+      link: '',
+      icon: 'github',
+    },
+    {
+      name: 'Facebook',
+      link: '',
+      icon: 'facebook',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {

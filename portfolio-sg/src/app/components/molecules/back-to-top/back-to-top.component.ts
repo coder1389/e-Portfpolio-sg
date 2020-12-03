8cd75e970933eb01ae1fe89df03617711b48bc84
Danijel Malinovic <danijelmalinovic2@gmail.com>
2020-12-03 10:28:31 +0100

    back to top button

--- portfolio-sg/src/app/components/molecules/back-to-top/back-to-top.component.ts
+++ portfolio-sg/src/app/components/molecules/back-to-top/back-to-top.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'sg-back-to-top',
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./back-to-top.component.scss']
 })
 export class BackToTopComponent implements OnInit {
+
+  @HostBinding('class.show') show: boolean;
+
+  @HostListener('window:scroll') onScroll() {
+    this.show = window.pageYOffset > 200
+  }
+
+  @HostListener('click') onClick() {
+    window.scrollTo({
+      top:0,
+      behavior: "smooth"
+    });
+  }
 
   constructor() { }
 
